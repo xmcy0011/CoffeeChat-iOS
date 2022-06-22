819a3335a99a30641b6baa094ca3f720b2fb5ca7
xmcy0011 <xmcy0011@sina.com>
2022-06-22 11:52:25 +0800

    init

---
+++ Coffchat/Coffchat/UI/Me/Setting/IMLoginoutTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  IMLoginoutTableViewCell.swift
+//  Coffchat
+//
+//  Created by fei.xu on 2020/4/15.
+//  Copyright © 2020 Coffeechat Inc. All rights reserved.
+//
+
+import UIKit
+
+class IMLoginoutTableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
